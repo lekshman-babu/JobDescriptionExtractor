e027f02d13f9401958833a547f81805d920f2bdb
lekshman-babu <118737588+lekshman-babu@users.noreply.github.com>
2022-11-27 21:27:46 +0530

    Update third_week.py

--- third_week.py
+++ third_week.py
@@ -1,13 +1,19 @@
+#opening file from second_week.py
 f=open("demo2.txt",'r')
+
 a=[]
 b=''
 skill_dictionary={}
+
+#reading each line the file
 while True:
     eof=f.readline()
     if eof=='':
         break
     if eof.isupper():
         if b!='':
+            
+            #replace first and last word with ''
             b=b.replace(" ",'',1)
             b=b.replace("\n",'')
             skill_dictionary[b]=a
@@ -15,11 +21,17 @@ while True:
         b=eof
     else:
         if eof.islower():
+            
+            #replace last word with ''
             eof=eof.replace("\n",'')
             a.append(eof)
+
+#created skill dictionary
 print("the dictionary keys are:")
 for i in skill_dictionary:
     print(i)
+
+#getting user input
 choice=input("choose on for the top: ")
 for i in skill_dictionary[choice]:
     print(i)
