18bcd5de53f3142d68987e54cc1b3de22ccb30c5
lekshman-babu <118737588+lekshman-babu@users.noreply.github.com>
2022-11-29 19:09:23 +0530

    Update fourth_week.py

--- fourth_week.py
+++ fourth_week.py
@@ -1,4 +1,6 @@
 import third_week as tw
+
+#iterating through the dictionary and the list
 for i in tw.skill_dictionary:
     count=0
     for j in tw.jobskill:
@@ -6,6 +8,8 @@ for i in tw.skill_dictionary:
             if i in k:
                 count+=1
     tw.skill_dictionary[i]=count
+    
+#displaying the new ranked list
 print("the ranked skill list")
 for i in sorted(tw.skill_dictionary,key=tw.skill_dictionary.get,reverse=True):
     print(f"{i} : {tw.skill_dictionary[i]}")
