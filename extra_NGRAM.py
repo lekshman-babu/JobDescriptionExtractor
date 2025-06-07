b978524b890b2d798fd1c21ca2ac8e2fc79c9615
lekshman-babu <118737588+lekshman-babu@users.noreply.github.com>
2025-06-07 16:19:44 -0700

    Delete extra_NGRAM.py

--- extra_NGRAM.py
+++
@@ -1,9 +0,0 @@
-def ngram(string,n):
-    a=string.split()
-    for i in range(len(a)):
-        for j in range(i,i+n):
-            print(a[j],end=' ')
-        if i+n>len(a)-1:
-            break
-        print()
-ngram("hi i am xyz my friends are yzx ",4)
